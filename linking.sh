d89acd0bcbf093de5f16f38282a74928672df159
ambergorzynski <amber.gorzynski@gmail.com>
2023-05-25 15:50:26 +0100

    add example opt pass

--- linking.sh
+++ linking.sh
@@ -2,6 +2,8 @@
 clang -emit-llvm -S wrapper.cpp -o wrapper.ll
 llvm-as run_cfg_2.ll -o run_cfg.bc
 llvm-as wrapper.ll -o wrapper.bc
-llvm-link run_cfg.bc wrapper.bc -o out.bc
+llvm-link run_cfg.bc wrapper.bc -o out_unopt.bc
+#opimisation step
+opt -passes=instcount out_unopt.bc -o out.bc
 llc -filetype=obj out.bc -o out.o
 clang++ out.o -o out
