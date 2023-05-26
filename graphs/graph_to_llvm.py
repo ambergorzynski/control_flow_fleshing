501d57dc3d993536ae0a3d9c9882b04b125585d3
ambergorzynski <amber.gorzynski@gmail.com>
2023-05-26 12:01:23 +0100

    create CFG class

--- graphs/graph_to_llvm.py
+++ graphs/graph_to_llvm.py
@@ -1,4 +1,24 @@
-import networkx
+import networkx as nx
 import pickle
 
-# import graph
+class CFG():
+
+    def __init__(self, graph : nx.MultiDiGraph):
+        self.raw_graph : nx.MultiDiGraph = graph
+        self.fleshed_graph : str = None
+
+    def fleshout(self) -> str:
+        ''' 
+            converts control flow graph to LLVM IR 
+            returns str containing LLVM IR program
+        '''
+
+    def save_llvm_to_file(self, filename : str) -> bool:
+        ''' 
+            writes CFG to given file in LLVM IR 
+            returns true if file write is successful
+            false otherwise
+        '''
+
+def main():
+    
