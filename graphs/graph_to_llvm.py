47d45d086b9440644d4c807fe503b19304bf6d67
ambergorzynski <amber.gorzynski@gmail.com>
2023-05-26 13:05:56 +0100

    iterate over graph

--- graphs/graph_to_llvm.py
+++ graphs/graph_to_llvm.py
@@ -4,7 +4,7 @@ import pickle
 class CFG():
 
     def __init__(self, graph : nx.MultiDiGraph):
-        self.raw_graph : nx.MultiDiGraph = graph
+        self.graph : nx.MultiDiGraph = graph
         self.fleshed_graph : str = None
 
     def fleshout(self) -> str:
@@ -14,18 +14,25 @@ class CFG():
             and saves as member variable
         '''
 
+        for n in self.graph:
+            print("Node: " + str(n) + " has child nodes: ")
+            for v in self.graph.adj[n]:
+                print(v)
+
+
     def save_llvm_to_file(self, filename : str) -> bool:
         ''' 
             writes CFG to given file in LLVM IR 
             returns true if file write is successful
             false otherwise
         '''
+        pass
 
 def main():
 
     # hard coded example graph - next step is to use cmd line args
     
-    graph1 = pickle.load(open("graph1.p", "rb"))
+    graph1 = pickle.load(open("graph_1.p", "rb"))
 
     cfg = CFG(graph1)
 
