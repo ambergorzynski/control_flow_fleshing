2317a89e4ea762a3fb1acfb0aaac5575776c7065
ambergorzynski <amber.gorzynski@gmail.com>
2023-05-26 12:09:13 +0100

    add main fn

--- graphs/graph_to_llvm.py
+++ graphs/graph_to_llvm.py
@@ -11,6 +11,7 @@ class CFG():
         ''' 
             converts control flow graph to LLVM IR 
             returns str containing LLVM IR program
+            and saves as member variable
         '''
 
     def save_llvm_to_file(self, filename : str) -> bool:
@@ -21,4 +22,20 @@ class CFG():
         '''
 
 def main():
+
+    # hard coded example graph - next step is to use cmd line args
     
+    graph1 = pickle.load(open("graph1.p", "rb"))
+
+    cfg = CFG(graph1)
+
+    cfg.fleshout()
+
+    if (cfg.save_llvm_to_file("run_cfg_wip.ll")):
+        print("Fleshed CFG created successfully!")
+    
+    else:
+        print("Problem saving fleshed CFG")
+
+if __name__=="__main__":
+    main()
