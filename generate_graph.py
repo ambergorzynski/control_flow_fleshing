f5bb1366c9fc118f61120b4881db4ef5eb8f5e11
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-15 12:33:38 +0100

    plot graph

--- generate_graph.py
+++ generate_graph.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import pickle
+import matplotlib.pyplot as plt
+import pydot
 
 class GraphGenerator():
 
@@ -73,19 +75,40 @@ class GraphGenerator():
         
         return G
 
+    def generate_graph(self, n_nodes) -> nx.MultiDiGraph:
+
+        ''' returns graph with number of nodes specified '''
+
+        # generate graph with given number of nodes
+
+        # loop over nodes and create edges
+
+        # final node should have 0 successors (exit node)
+
 def main():
 
+    
     generator = GraphGenerator()
     G1 = generator.preset_graph_1()
     G2 = generator.preset_graph_2()
     G3 = generator.preset_graph_3()
     G4 = generator.preset_graph_4()
-
+    '''
     #pickle graphs (for now) - change this to JSON rather than pickle later
     pickle.dump(G1, open("graph_1.p", "wb"))
     pickle.dump(G2, open("graph_2.p", "wb"))
     pickle.dump(G3, open("graph_3.p", "wb") )
     pickle.dump(G4, open("graph_4.p", "wb") )
+    '''
+
+    nx.draw_networkx(G3)
+    plt.show()
+    '''
+    graph = nx.drawing.nx_pydot.to_pydot(G1)
+    graph.write_png("output.png")
+
+    print(G1)
+    '''
 
 if __name__=="__main__":
     main()
