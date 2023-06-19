da49615c1cc1631cd59e3dc970f5cf9a4ab6ccd5
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-19 11:28:27 +0100

    add fleshing step to fuzzing campaign

--- fuzzing_campaign.py
+++ fuzzing_campaign.py
@@ -1,10 +1,9 @@
 import networkx as nx
 import pickle
-import sys
-import random
 from datetime import datetime
 from random import Random
 from generate_graph import GraphGenerator
+from CFG import CFG
 
 def generate_graphs(n_graphs, filepath, min_graph_size, max_graph_size, seed):
 
@@ -22,11 +21,26 @@ def generate_graphs(n_graphs, filepath, min_graph_size, max_graph_size, seed):
 
         pickle.dump(G, open(f'{filepath}graph_{i}.p', "wb"))
 
+def flesh_graphs(n_graphs, graph_filepath, llvm_filepath):
+
+    for i in range(n_graphs):
+        
+        G = pickle.load(open(f'{graph_filepath}graph_{i}.p', "rb"))
+
+        cfg = CFG(G)
+
+        cfg.fleshout()
+
+        cfg.save_llvm_to_file(f'{llvm_filepath}run_cfg_{i}.ll')
+
+
+
 def main():
 
     # input parameters
     n_graphs = 10
     graph_filepath = "graphs/fuzzing_190623/"
+    llvm_filepath = "test_input_llvm_programs/fuzzing_190623/"
     min_graph_size = 20
     max_graph_size = 500
     max_path_length = 900
@@ -36,6 +50,7 @@ def main():
     generate_graphs(n_graphs, graph_filepath, min_graph_size, max_graph_size, seed)
 
     # Step 2 : flesh graphs
+    flesh_graphs(n_graphs, graph_filepath, llvm_filepath)
 
     # Step 3 : generate paths for each graph
 
