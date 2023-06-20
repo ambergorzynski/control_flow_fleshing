951aa8266dc05d941bf5b223f5911054e0ae98d4
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-20 11:36:35 +0100

    made filepaths member variables of Tester

--- run_test.py
+++ run_test.py
@@ -2,44 +2,46 @@ import subprocess
 
 class Tester():
 
-    def __init__(self):
-        pass 
+    def __init__(self, test_filepath, input_filepath, output_filepath):
+        self.test = test_filepath
+        self.out = output_filepath
+        self.input = input_filepath
 
-    def compile(self, test_filepath, output_filepath, test_name):
+    def compile(self, test_name):
 
         # compile c++ wrapper to llvm ir
-        cmd = [f'clang -emit-llvm -S run_test.cpp -o {output_filepath}/run_test.ll']
+        cmd = [f'clang -emit-llvm -S run_test.cpp -o {self.out}/run_test.ll']
         result = subprocess.run(cmd, shell=True)
 
         # assemble wrapper llvm ir to llvm bc
         # (could do previous two steps in one go)
-        cmd = [f'llvm-as {output_filepath}/run_test.ll -o {output_filepath}/run_test.bc']
+        cmd = [f'llvm-as {self.out}/run_test.ll -o {self.out}/run_test.bc']
         result = subprocess.run(cmd, shell=True)
 
         # assemble llvm ir test program to llvm bc
-        cmd = [f'llvm-as {test_filepath}/{test_name}.ll -o {output_filepath}/{test_name}.bc']
+        cmd = [f'llvm-as {self.test}/{test_name}.ll -o {self.out}/{test_name}.bc']
         result = subprocess.run(cmd, shell=True)
 
         # link wrapper with test program
-        cmd = [f'llvm-link {output_filepath}/{test_name}.bc {output_filepath}/run_test.bc -o {output_filepath}/{test_name}_out_unopt.bc']
+        cmd = [f'llvm-link {self.out}/{test_name}.bc {self.out}/run_test.bc -o {self.out}/{test_name}_out_unopt.bc']
         result = subprocess.run(cmd, shell=True)
 
         # perform any optimisations
         # single 'instcount' opt hard-coded for now
-        cmd = [f'opt -passes=instcount {output_filepath}/{test_name}_out_unopt.bc -o {output_filepath}/{test_name}_opt.bc']
+        cmd = [f'opt -passes=instcount {self.out}/{test_name}_out_unopt.bc -o {self.out}/{test_name}_opt.bc']
         result = subprocess.run(cmd, shell=True)
 
         # generate object file
-        cmd = [f'llc -filetype=obj {output_filepath}/{test_name}_opt.bc -o {output_filepath}/{test_name}_out_opt.o']
+        cmd = [f'llc -filetype=obj {self.out}/{test_name}_opt.bc -o {self.out}/{test_name}_out_opt.o']
         result = subprocess.run(cmd, shell=True)
 
         # generate executable
-        cmd = [f'clang++ {output_filepath}/{test_name}_out_opt.o -o {output_filepath}/{test_name}_out']
+        cmd = [f'clang++ {self.out}/{test_name}_out_opt.o -o {self.out}/{test_name}_out']
         result = subprocess.run(cmd, shell=True)
 
-    def execute(self, input_filepath, output_filepath, test_name, path_name):
+    def execute(self, test_name, path_name):
         
-        cmd = [f'./{output_filepath}/{test_name}_out {input_filepath}/{path_name}.txt']
+        cmd = [f'./{self.out}/{test_name}_out {self.input}/{path_name}.txt']
         result = subprocess.run(cmd, shell=True)
         print(result.returncode)
         
@@ -50,19 +52,18 @@ class Tester():
 
 def main():
 
-    test = Tester()
-
     base = 'fuzzing/fuzzing_200623'
     test_filepath = f'{base}/llvm'
     input_filepath = f'{base}/input'
     output_filepath = f'{base}/running'
     test_name = 'run_cfg_0'
     path_name = 'input_graph_0_path0'
+
+    test = Tester(test_filepath, input_filepath, output_filepath)
     
+    test.compile(test_name)
 
-    #test.compile(test_filepath, output_filepath, test_name)
-
-    test.execute(input_filepath, output_filepath, test_name, path_name)
+    test.execute(test_name, path_name)
 
 if __name__=="__main__":
     main()
