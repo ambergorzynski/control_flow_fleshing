ec821c9fba33ed64d782281969626dc60a779f35
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-20 09:25:38 +0100

    set up run_test framework for compilation and execution

---
+++ run_test.py
@@ -0,0 +1,16 @@
+import subprocess
+
+class Tester():
+
+    def __init__(self):
+        pass 
+
+    def compile(self):
+        pass
+
+    def execute(self):
+        pass
+
+    def parse_results(self):
+        pass
+
