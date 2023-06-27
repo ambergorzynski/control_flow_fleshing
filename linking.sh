85c80e542d599700f189dcf9b807009614ff0335
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-27 12:46:51 +0100

    streamline compilation process

--- linking.sh
+++ linking.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 clang -emit-llvm -S run_test.cpp -o run_test.ll
-llvm-as test_input_llvm_programs/run_cfg_wip_graph_test.ll -o run_cfg.bc
-llvm-as run_test.ll -o run_test.bc
-llvm-link run_cfg.bc run_test.bc -o out_unopt.bc
-#opimisation step
-opt -passes="adce" out_unopt.bc -o out.bc
-llc -filetype=obj out.bc -o out.o
+llvm-link test_input_llvm_programs/run_cfg_wip_graph_test.ll run_test.ll -o out_unopt.ll
+opt -passes="adce" out_unopt.ll -o out.ll
+llc -filetype=obj out.ll -o out.o
 clang++ out.o -o out
+#llvm-as test_input_llvm_programs/run_cfg_wip_graph_test.ll -o run_cfg.bc
+#llvm-as run_test.ll -o run_test.bc
+#llvm-link run_cfg.bc run_test.bc -o out_unopt.ll
+#opt -passes="adce" out_unopt.ll -o out.ll
+#clang++ out.bc -o out
+#llc -filetype=obj out.bc -o out.o
+#clang++ out.o -o out
