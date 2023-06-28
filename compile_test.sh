1a3266bbe6c4c10c2949e88e216b52b77cf31a18
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-28 13:25:59 +0100

    emit textual llvm ir from optimisation

--- compile_test.sh
+++ compile_test.sh
@@ -4,7 +4,7 @@ llvm=$2
 name=$3
 optimisations=$4
 clang++ -c run_test.cpp -o $output/run_test.o
-opt -passes=$optimisations $llvm/${name}.ll -o $output/${name}_opt.ll
+opt -passes=$optimisations -S $llvm/${name}.ll -o $output/${name}_opt.ll
 llc -filetype=obj $output/${name}_opt.ll
 clang++ $output/run_test.o $output/${name}_opt.o -o $output/${name}_out
 
