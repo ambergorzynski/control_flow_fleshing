85f7d9befa3979f51a3da782fd0c46a0eb09cc61
ambergorzynski <amber.gorzynski@gmail.com>
2023-06-28 12:47:22 +0100

    shell script to compile & link llvm and c++ progs

---
+++ compile_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+output=$1
+llvm=$2
+name=$3
+optimisations=$4
+clang++ -c run_test.cpp -o $output/run_test.o
+opt -passes=$optimisations $llvm/${name}.ll -o $output/${name}_opt.ll
+llc -filetype=obj $output/${name}_opt.ll
+clang++ $output/run_test.o $output/${name}_opt.o -o $output/out
+
