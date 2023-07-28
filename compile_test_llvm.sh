f6a5d9e9451e25a8fe94349f87e59ca0c2356a72
ambergorzynski <amber.gorzynski@gmail.com>
2023-07-28 11:19:40 -0400

    renaming

--- compile_test_llvm.sh
+++ compile_test_llvm.sh
@@ -6,5 +6,5 @@ optimisations=$4
 #clang++ -c run_test.cpp -o $output/run_test.o
 opt -passes=$optimisations -S $llvm/${name}.ll -o $output/${name}_opt.ll
 llc -filetype=obj $output/${name}_opt.ll
-clang++ $output/run_test.o $output/${name}_opt.o -o $output/${name}_out
+clang++ $output/Wrapper.o $output/${name}_opt.o -o $output/${name}_out
 
