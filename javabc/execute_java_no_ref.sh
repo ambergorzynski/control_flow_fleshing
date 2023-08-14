39ec13494e5f8d936b0b45cd80b75d5efeb1a84e
ambergorzynski <amber.gorzynski@gmail.com>
2023-08-14 09:59:36 +0100

    updated dir for java compilation

--- javabc/execute_java_no_ref.sh
+++ javabc/execute_java_no_ref.sh
@@ -5,5 +5,5 @@ path_name=$3
 output_filename=$4
 bad_output_filename=$5
 n_function_repeats=$6
-cd $src
-java -cp .:./testing/test$test_number testing/Wrapper "paths/$path_name.txt" "output/$output_filename.txt" "output/$bad_output_filename.txt" $n_function_repeats
+cd $src/testing
+java -cp .:./test$test_number Wrapper "../paths/$path_name.txt" "../output/$output_filename.txt" "output/$bad_output_filename.txt" $n_function_repeats
