e0e45b937825e683898b646025c3bd5b71a743ef
ambergorzynski <amber.gorzynski@gmail.com>
2023-08-18 16:51:33 +0100

    clang no pie

--- llvm/compile_test_llvm.sh
+++ llvm/compile_test_llvm.sh
@@ -5,5 +5,5 @@ name=$3
 optimisations=$4
 opt -passes=$optimisations -S $llvm/${name}.ll -o $output/${name}_opt.ll
 llc -filetype=obj $output/${name}_opt.ll
-clang++ $output/Wrapper.o $output/${name}_opt.o -o $output/${name}_out
+clang++ -no-pie $output/Wrapper.o $output/${name}_opt.o -o $output/${name}_out
 
