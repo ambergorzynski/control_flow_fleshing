764c12b0918751a057cff786ff15a619e11f29ad
ambergorzynski <amber.gorzynski@gmail.com>
2023-08-29 16:07:13 +0100

    test name typo

--- c/compile_test_c.sh
+++ c/compile_test_c.sh
@@ -4,4 +4,4 @@ program_path=$2
 test_name=$3
 optimisations=$4
 
-clang++ $program_path/$test_name.c $output_path/Wrapper.cpp -o $output_path/$test_name_out $optimisations
+clang++ $program_path/$test_name.c $output_path/Wrapper.cpp -o $output_path/${test_name}_out $optimisations
