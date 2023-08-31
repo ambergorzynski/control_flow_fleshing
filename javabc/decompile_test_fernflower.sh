26d9ae72a6b3dcb9380abd7e2ea5f119e5c115e1
agg22 <amber.gorzynski22@imperial.ac.uk>
2023-08-31 12:20:57 +0100

    remove previuos compilation from fernflower

--- javabc/decompile_test_fernflower.sh
+++ javabc/decompile_test_fernflower.sh
@@ -6,3 +6,6 @@ decompiler_jar=$4
 
 cd $testing
 ${jvm}/java -jar ${decompiler_jar} testing/$test_name/TestCase.class testing/$test_name/
+
+# remove previous TestCase.class so it can be replaced (or not, if re-compilation fails)
+rm -f testing/$test_name/TestCase.class
