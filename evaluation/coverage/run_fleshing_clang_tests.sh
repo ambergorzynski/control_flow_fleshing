5b8c14b9dd9d24a3fd229d58924e1f9e07f4b329
ambergorzynski <user42@cloud-vm-43-155.doc.ic.ac.uk>
2023-09-04 13:35:50 +0100

    add runner for ll coverage tests with clang

---
+++ evaluation/coverage/run_fleshing_clang_tests.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# filepaths
+base=/home/user42/amber-demo
+gfauto=/home/user42/amber-demo/graphicsfuzz/gfauto
+compiler_build=$base/llvm-project/
+llvm_path=$base/llvm-project/build/bin
+working_folder=$base/cfg
+
+# test-specific filepaths
+cov_name=coverage_fleshing_tests_clang
+info_name=${cov_name}_output.info
+out_name=${cov_name}_out
+gcda_folder=$base/cfg/$cov_name/
+coverage_folder=$base/cfg/${cov_name}_gfauto
+
+
+# set gcov prefix
+export GCOV_PREFIX=$gcda_folder
+
+echo 'Running tests...'
+
+source /home/user42/amber-demo/cfg/repo/venv/bin/activate
+
+# run tests
+
+python run_fleshing_tests_clang.py $llvm_path
+
+# unset
+unset GCOV_PREFIX
+
+deactivate
+
+echo 'Finished running tests. Starting coverage...'
+
+# run gfauto to create symlinks
+# TODO: extract script to do this from gfauto source code
+mkdir -p $coverage_folder
+mkdir -p $coverage_folder/function
+mkdir -p $coverage_folder/line
+
+source $gfauto/.venv/bin/activate
+
+cd $working_folder/coverage_processed/line
+gfauto_cov_from_gcov --out run_gcov2cov.cov $compiler_build --gcov_prefix_dir $gcda_folder --num_threads 32 --gcov_uses_json
+
+deactivate
+
+# create the coverage report with lcov
+
+cd $working_folder
+/home/user42/my_lcov/bin/lcov --capture --directory ${cov_name} --output-file $info_name 
+genhtml $info_name --output-directory $out_name
+
+echo 'Coverage complete!'
+cd /home/user42/amber-demo/cfg/repo/evaluation/coverage
--- evaluation/coverage/run_fleshing_tests_clang.py
+++ evaluation/coverage/run_fleshing_tests_clang.py
@@ -9,16 +9,24 @@ def main():
     
     args = parser.parse_args()
 
-    test_folder = '/home/user42/amber-demo/fleshing_progs'
+    test_folder = '/home/user42/amber-demo/flesh_tests_for_clang'
 
+    for i in range(100):
+        for j in range(10):
+            print(f'running opt on test {i}, {j}')
+            result = subprocess.run(f'''{args.clang_path}/clang -c {test_folder}/run_cfg_{i}_path_{j}.ll -o {test_folder}/run_cfg_{i}_path{j}.o -O3''', shell=True)
+
+    
+    """
     for i in range(100):
         for j in range(10):
             try:
                 print(f'running opt on test {i}, {j}')
-                result = subprocess.run(f'''{args.clang_path}/clang -c {test_folder}/run_cfg_{i}_path{j}.ll -o {test_folder}/run_cfg_{i}_path{j}.o -O3''', shell=True, timeout = 5)
+                result = subprocess.run(f'''{args.clang_path}/clang -c {test_folder}/run_cfg_{i}_path_{j}.ll -o {test_folder}/run_cfg_{i}_path{j}.o -O3''', shell=True, timeout = 5)
 
             except subprocess.TimeoutExpired:
                 print(f'Timeout expired for {i}, {j}')
+    """
 
 if __name__=="__main__":
     main()
