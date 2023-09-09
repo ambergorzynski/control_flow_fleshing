b0482448d0e2e21bb1ae17d3a827a594d3cc9d56
ambergorzynski <amber.gorzynski@gmail.com>
2023-09-09 09:38:41 +0100

    minor

--- evaluation/coverage/mutation_analysis.py
+++ evaluation/coverage/mutation_analysis.py
@@ -10,8 +10,14 @@ def main():
              'work_csmith_loop-simplifycfg',
              'work_csmith_loop-unroll',
              'work_csmith_loop-unroll-and-jam']
-
-    for name in names:
+    f_names = ['work_flesh_adce',
+             'work_flesh_default<O3>',
+             'work_flesh_lcssa',
+             'work_flesh_loop-rotate',
+             'work_flesh_loop-simplifycfg',
+             'work_flesh_loop-unroll',
+             'work_flesh_loop-unroll-and-jam']
+    for name in f_names:
         folder = f'{directory}/{name}/killed_mutants/'
 
         sub_folders = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
