c9ab3dc7de4e05f02bc25811a28fbdbb610247ca
agg22 <amber.gorzynski22@imperial.ac.uk>
2023-09-10 09:19:57 +0100

    throughput measurement

---
+++ throughput_measurement.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def javabc():
+    pass 
+
+def llvm():
+    pass
+
+def cil():
+    pass
+
+def c():
+    pass
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
