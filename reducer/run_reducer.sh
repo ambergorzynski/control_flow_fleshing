22e5c2aafbd4f7f496786e36e2355fe423012de8
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-02-08 14:19:15 +0000

    reducer folder path change

--- reducer/run_reducer.sh
+++ reducer/run_reducer.sh
@@ -1,4 +1,4 @@
 python run_reducer.py \
-    '/data/dev/fuzzflesh/graphs/graph_test.p' \
-    '/data/dev/fuzzflesh/graphs/inputs.txt' \
+    '/data/work/ghidra/reducer/test_graph.p' \
+    '/data/work/ghidra/reducer/inputs.txt' \
     '/data/work/ghidra/reducer/interesting.sh'
