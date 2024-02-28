5e875a8ba2fdf36e88fc766e18195722f3b14a48
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-02-28 09:42:50 +0000

    add merge pass for nodes on path

--- reducer/passes/merge.py
+++ reducer/passes/merge.py
@@ -11,10 +11,11 @@ from passes.abstract import AbstractPass
 # this is more complicated because it requires adjusting the
 # path as well as the graph
 
-class MergePass(AbstractPass):
+class MergeOffPathPass(AbstractPass):
     '''
         This pass merges nodes that are not on the path, and 
         not connected to any nodes on the path by an edge.
+        It modifies the graph but not the path.
     '''
 
     def __init__(self):
@@ -23,7 +24,7 @@ class MergePass(AbstractPass):
 
     def new(self, cfg : CFG, path : Path) -> None:
         
-        self.nodes = self.get_merge_nodes(cfg, path)
+        self.nodes = self.get_off_path_nodes(cfg, path)
 
     def check_prerequisites(self, cfg : CFG, path : Path) -> bool:
         
@@ -47,11 +48,11 @@ class MergePass(AbstractPass):
      
             cfg = cfg.merge_nodes(node1, node2)
 
-        # path is not modified here - we are only merging off-path nodes
+        print(f'cfg size after merge transform is {cfg.total_nodes()}')
 
         return (cfg, path)
 
-    def get_merge_nodes(self, cfg : CFG, path : Path) -> list[int]:
+    def get_off_path_nodes(self, cfg : CFG, path : Path) -> list[int]:
 
         merge_nodes = [x for x in cfg.nodes if x not in cfg.get_exit_nodes()
                 and x not in cfg.get_path_neighbours(path)
@@ -59,55 +60,152 @@ class MergePass(AbstractPass):
 
         return merge_nodes
 
-    def update_path(self, old_cfg : CFG, old_path : Path, node1 : int, node2 : int) -> Path:
- 
+class MergeOnPathPass(AbstractPass):
+    '''
+        This pass merges nodes that are on the path, or
+        not connected to any nodes on the path by an edge.
+        It modifies the graph and the path.
+    '''
+
+    def __init__(self):
+        self.chunk : int = 2 
+        self.nodes : list[int] = []
+
+    def new(self, cfg : CFG, path : Path) -> None:
+        
+        self.nodes = self.get_on_path_nodes(cfg, path)
+
+    def check_prerequisites(self, cfg : CFG, path : Path) -> bool:
+        
+        if len(self.nodes) < 2:
+            return False
+
+        return True
+
+    def transform(self, cfg : CFG, path : Path) -> tuple[CFG, Path]:
+
+        n = len(self.nodes) // self.chunk if len(self.nodes) >= self.chunk else 1
+        for i in range(n):
+
+            # pop second node because it will be destroyed in the 
+            # merge, so we cannot merge on it again
+            node1 = self.nodes[0]
+            node2 = self.nodes.pop(1)
+           
+            print(f'nodes for merging: {node1} {node2}')
+            
+            new_cfg = cfg.merge_nodes(node1, node2)
+
+            new_path = self.update_path(new_cfg, cfg, path, node1, node2)
+            
+            '''
+            print(f'cfg : {new_cfg.get_nodes()}')
+            print(f'cfg : {new_cfg.get_edges()}')
+            print(f'path : {new_path.expected_output}')
+            print(f'dirs : {new_path.directions}')
+            '''
+
+            # update cfg and path to modified
+            cfg = new_cfg
+            path = new_path
+        
+        print(f'cfg size after merge transform is {cfg.total_nodes()}')
+        
+        return (new_cfg, new_path)
+
+    def get_on_path_nodes(self, cfg : CFG, path : Path) -> list[int]:
+
+        merge_nodes = [x for x in cfg.nodes if x not in cfg.get_exit_nodes()
+                and x != 0
+                and (x in path.expected_output
+                or x in cfg.get_path_neighbours(path))]
+
+        return merge_nodes
+
+    def update_path(self, cfg : CFG, old_cfg : CFG, old_path : Path, node1 : int, node2 : int) -> Path:
+
         new_path = Path(directions=old_path.directions, expected_output=old_path.expected_output)
       
         new_path.expected_output = [node1 if x == node2 else x for x in old_path.expected_output]
  
         full_direction_array = self.get_full_directions(old_cfg, old_path)
-
-        print(f'full direction array: {full_direction_array}')
-
-        # update node 1 directions
-        if node1 in old_path.expected_output:
-
-            if old_cfg.get_out_degree(node1) == 1:
-                
-                full_direction_array = [0 if old_path.expected_output[i] == node1 else x 
-                        for i, x in enumerate(full_direction_array)]
-
-            elif old_cfg.get_out_degree(node1) > 1:
-                pass # included for clarity - no need to modify if direction already existed
         
-            else:
-                pass #TODO: add
-
-        # update node 2 directions
-        if node2 in old_path.expected_output:
-
-            if old_cfg.get_out_degree(node2) == 1:
-                
-                new_direction = old_cfg.get_out_degree(node1)
-                
-                full_direction_array = [new_direction 
-                        if old_path.expected_output[i] == node2 else x 
-                        for i, x in enumerate(full_direction_array)]
-
-            elif old_cfg.get_out_degree(node2) > 1:
-                
-                full_direction_array = [(old_cfg.get_out_degree(node1) + x) 
-                        if old_path.expected_output[i] == node2 else x 
-                        for i, x in enumerate(full_direction_array)]
-            
-            else:
-                pass #TODO: add
-
-        print(f'full direction array after updates: {full_direction_array}')
+        former_direction_array = full_direction_array
         
+        # find all nodes in the output that require directions adjustments
+        # this includes node1 and any node that was a parent of node2
+        nodes = set(old_cfg.get_parents(node2) + [node1])
+
+        node_indices = [i for i, x in enumerate(new_path.expected_output) if x in nodes]
+
+        for i in node_indices:
+
+            # replace out-direction
+            full_direction_array[i] = self.get_new_direction_using_adj(
+                    cfg,
+                    new_path.expected_output[i],
+                    new_path.expected_output[i+1]
+                    )
+
+            #print(f'replace out direction of node {new_path.expected_output[i]} to {new_path.expected_output[i+1]} with dir {full_direction_array[i]}')
+
+            # also replace in-direction if it is node1
+            if new_path.expected_output[i] == node1:
+                full_direction_array[i-1] = self.get_new_direction_using_adj(
+                        cfg, 
+                        new_path.expected_output[i-1], 
+                        node1
+                        )
+
+        former = list(zip(old_path.expected_output, former_direction_array))
+        zipped = list(zip(new_path.expected_output, full_direction_array))
+
+        #print(f'former:\n {former}')
+        #print(f'current:\n {zipped}')
+
+        new_path.directions = [x for x in full_direction_array if x != None]
 
         return new_path
 
+    def get_new_direction_using_adj(self, new_cfg : CFG, u : int, v : int) -> int:
+
+        '''
+            Returns the direction required to reach successor node v from u.
+            This is consistent with the rest of the program generation process,
+            which is based on successor nodes rather than edges.
+        '''
+
+        #TODO: change the rest of the program generation process to be based on edges
+
+        successors = new_cfg.get_successors(u)
+
+        if len(successors) == 1:
+            return None
+
+        #print(f'successors from {u} are: {successors}')
+        #print(f'{u} to {v} is successor index {successors.index(v)}')
+
+        return successors.index(v)
+
+    def get_new_direction_using_edges(self, new_cfg : CFG, u : int, v : int) -> int:
+
+        ''' 
+            Returns the direction required to follow edge (u,v). If multiple
+            edges (u,v) exist, then it returns the first direction. If u has
+            only one out edge, then the direction returned is None since
+            no direction is required to traverse (u,v)
+        '''
+
+        edges = new_cfg.get_out_edges(u)
+
+
+        if len(edges) == 1:
+            return None
+        print(f'out edges from {u} is {edges}')
+        print(f'{u} to {v} is {edges.index((u,v))}')
+
+        return edges.index((u,v))
+            
     def get_full_directions(self, cfg : CFG, path : Path) -> list[int]:
         '''
         create direction array that corresponds in size to output; all nodes on output 
@@ -115,8 +213,11 @@ class MergePass(AbstractPass):
         '''
 
         # get degree of each node on path
-        degree = [cfg.get_out_degree(x) for x in path.expected_output]
+        #degree = [cfg.get_out_degree(x) for x in path.expected_output]
         
+        # get n successors of each node on path
+        degree = [cfg.successors(x) for x in path.expected_output]
+
         # get appropriate direction 
         direction = []
         dir_index = 0
