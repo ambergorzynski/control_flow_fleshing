4c144f46b3f82cef7ad9c19041cfa5bf8fa1d758
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-06-04 14:50:54 +0100

    Add ProgramFlesher outline

--- src/fuzzflesh/program_generator/flesher.py
+++ src/fuzzflesh/program_generator/flesher.py
@@ -20,3 +20,68 @@ class ProgramFlesher(ABC):
         
         except:
             return False
+        
+    def fleshout(self, cfg : CFG, prog_number=None) -> List[InstructionBlock]:
+
+        '''
+        Converts control flow graph to target language.
+        Returns a list of instructions that constitue the
+        program.
+        '''
+
+        program : List[InstructionBlock] = []
+
+        program.append(self.flesh_program_start())
+
+        program.append(self.flesh_program_body(cfg))
+
+        program.append(self.flesh_program_end())
+
+        return program
+    
+    def flesh_program_body(self, cfg : CFG) -> List[InstructionBlock]:
+
+        body : List[InstructionBlock] = []
+
+        for n in cfg.graph:
+
+            body.append(self.flesh_start_of_node(n))
+
+            n_successors = cfg.successors(n)
+
+            match n_successors:
+
+                case 0:
+                    body.append(self.flesh_exit_node(n))
+                case 1:
+                    body.append(self.flesh_unconditional_node(n))
+                case 2:
+                    body.append(self.flesh_conditional_node(n))
+                case _:
+                    body.append(self.flesh_switch_node(n))
+
+        return body
+    
+    @abstractmethod
+    def flesh_program_start(self) -> List[InstructionBlock]:
+        pass
+            
+    @abstractmethod
+    def flesh_program_end(self) -> List[InstructionBlock]:
+        pass    
+    
+    @abstractmethod
+    def flesh_exit_node(self, n : int) -> List[InstructionBlock]:
+        pass    
+    
+    @abstractmethod
+    def flesh_unconditional_node(self, n : int) -> List[InstructionBlock]:
+        pass    
+    
+    @abstractmethod
+    def flesh_conditional_node(self, n : int) -> List[InstructionBlock]:
+        pass
+    
+    @abstractmethod
+    def flesh_switch_node(self, n : int) -> List[InstructionBlock]:
+        pass
