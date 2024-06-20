679fc1100836b474015d4d42a8c0968fc3756f57
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-06-20 16:35:07 +0100

    Move InstructionBlock to common.utils

--- src/fuzzflesh/common/utils.py
+++ src/fuzzflesh/common/utils.py
@@ -10,3 +10,13 @@ class Compiler(Enum):
     PROCYON = 3
     GRAALVM = 4
     HOTSPOT = 5
+
+class InstructionBlock():
+
+    def __init__(self, _code : str):
+        code = _code
+    
+    @abstractmethod
+    def write(self) -> str:
+        return code
+
