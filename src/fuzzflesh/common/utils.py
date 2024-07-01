761d2d34d66a528a0d097fb133ee6c843589f08e
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-07-01 12:45:37 +0100

    Add program class for writing

--- src/fuzzflesh/common/utils.py
+++ src/fuzzflesh/common/utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pathlib import Path
 
 class Lang(Enum):
     JAVABC = 1
@@ -14,8 +15,32 @@ class Compiler(Enum):
 class InstructionBlock():
 
     def __init__(self, _code : str):
-        code = _code
+        self.code : str = _code
     
-    def write(self) -> str:
-        return code
+    def get_code(self) -> str:
+        return self.code
+    
+class Program():
+
+    def __init__(self, _lang : Lang, _prog : list[InstructionBlock]):
+        self.program : list[InstructionBlock] = _prog
+        self.language : Lang = _lang
+
+    def write_to_file(self, filepath : Path, filename : str) -> bool:
+
+        with open(Path(filepath, f'{filename}{self.get_extension()}'), 'w') as f:
+            for i in self.program:
+                f.write(f'{i.get_code()}\n')
+        
+        return True
+    
+    def get_extension(self) -> str:
+        match self.language:
+            case Lang.JAVABC:
+                return '.j'
+            case Lang.C:
+                return '.c'
+            case _ : 
+                return '.txt'
+
 
