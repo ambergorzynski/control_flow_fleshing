4459e444b6ce7aae30db05ef022f802e0d570f96
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-07-04 12:33:21 +0100

    Add template run script

---
+++ run_template.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SRC='/data/dev/fuzzflesh/src'
+ACTION='fuzz'
+OUTPUT='/data/work/fuzzflesh/dev_testing/output'
+LANG='javabc'
+COMPILER='cfr'
+JVM='/usr/lib/jvm/java-19-openjdk-amd64/bin'
+JASMIN='/homes/agg22/dev/jasmin-2.4'
+JSON='/data/dev/java/json-simple-1.1.1.jar'
+DECOMPILER_PATH='/vol/bitbucket/agg22/cfr-0.152.jar'
+
+. $SRC/venv/bin/activate
+
+PYTHONPATH=$SRC/fuzzflesh
+
+python3 -m fuzzflesh \
+    $ACTION \
+    $BASE \
+    --dirs \
+    -graphs=3 \
+    -paths=2 \
+    $LANG \
+    $COMPILER \
+    $JVM \
+    $JASMIN \
+    $JSON \
+    $DECOMPILER_PATH 
+
