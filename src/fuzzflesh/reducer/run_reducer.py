aa300bd587625f8b86a8e3d9d21841efc4f80cf0
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-08-06 06:27:14 +0100

    Update reducer Path to Route class and inputs from txt to json

--- src/fuzzflesh/reducer/run_reducer.py
+++ src/fuzzflesh/reducer/run_reducer.py
@@ -5,31 +5,37 @@ import subprocess
 import random
 import time
 import networkx as nx
+import json
 
 from pathlib import Path
 
 sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 
-from CFG import CFG, Path
+from CFG import CFG, Route
 from reducer import Reducer
 
-def get_path(filename : str) -> Path:
+def get_path(filename : Path) -> Route:
     
     with open(filename, "r") as f:
-        content = f.readlines()
+        content = json.load(f)
 
-    directions = [int(i) for i in content[2].split()]
+    directions = content['dirs']
 
-    expected_output = [int(i) for i in content[3].split()]
+    expected_output = content['expected_output']
 
-    return Path(expected_output = expected_output, directions = directions)
+    return Route(expected_output = expected_output, directions = directions)
 
 if __name__==("__main__"):
     parser = argparse.ArgumentParser()
-    parser.add_argument("graph")
-    parser.add_argument("input")
-    parser.add_argument("interestingness_test")
-    parser.add_argument("output_path")
+
+    parser.add_argument("graph",
+                        help='Filepath to graph of interest.')
+    parser.add_argument("input",
+                        help='Filepath to input.json of path')
+    parser.add_argument("interestingness_test",
+                        help='Filepath to interestingness shell script.')
+    parser.add_argument("output_path",
+                        help='Filepath to which the reduced program should be saved.')
     parser.add_argument("--language", default="c")
    
     args = parser.parse_args()
@@ -45,4 +51,7 @@ if __name__==("__main__"):
     
     #reducer.reduce(['merge_off_path','remove_edge','merge_on_path'])
     
-    reducer.reduce(['merge_off_path', 'remove_edge', 'merge_on_path','merge_exit'])
+    reducer.reduce(['merge_off_path', 
+                'remove_edge', 
+                'merge_on_path',
+                'merge_exit'])
