15e78d9b88672a61a4f063e0866e9da46caee593
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-05-06 18:11:00 +0100

    Update packaging

--- src/analysis/coverage/cpp/fuzzflesh/run_coverage_analysis.sh
+++ src/analysis/coverage/cpp/fuzzflesh/run_coverage_analysis.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-
-# Unzip the coverage instrumented decompiler 
-unzip /data/dev/external/coverage/ghidra_cov_11.zip -d /data/dev/external/coverage/
+TIMELIMIT=$1
 
 # First, run the fuzzer to generate test classes for the given time budget
-TIMELIMIT=1
-
-/bin/bash run_ff_config.sh $TIMELIMIT
+./run_ff_config.sh $TIMELIMIT
 
 # Next, run the test classes using the decompiler instrumented with coverage
 ./get_coverage.sh
 
-# Remove the coverage instrumented decompiler for space reasons
-rm -rf /data/dev/external/coverage/ghidra_cov_11
