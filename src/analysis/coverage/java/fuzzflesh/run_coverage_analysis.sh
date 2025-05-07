95012455dc211707ad2275f5eb1ebe66821b354f
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-05-07 08:46:51 +0100

    Remove previous java coverage script

--- src/analysis/coverage/java/fuzzflesh/run_coverage_analysis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# First, run the fuzzer to generate test classes for the given time budget
-TIMELIMIT=1
-
-/bin/bash run_ff_all.sh $TIMELIMIT
-
-# Next, run the test classes using the decompiler instrumented with coverage
-./get_coverage.sh
